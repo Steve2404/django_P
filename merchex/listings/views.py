efc2af5b277f6bfc09e2e7277231bcae969abc8e
Steve2404 <jatsasteve@gmail.com>
2024-07-30 12:14:17 +0200

    creations de 03 instances de Band et d un nouveau models Listing

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 
+from listings.models import Band
+
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    band = Band.objects.all()
+    return HttpResponse(f"""
+    <h1> Hallo Django! </h1>
+    <p>Mes groupes preferes sont: </p>
+    <ul>
+        <li>Band 1: {band[0].name}</li>
+        <li>Band 2: {band[1].name}</li>
+        <li>Band 3: {band[2].name}</li>
+    </ul>
+    """)
 
 
 def about(request):
