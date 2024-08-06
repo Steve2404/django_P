06e52c2f8252af0e70324e1af59c748f52d49204
stevej <jatsasteve@gmail.com>
2024-08-06 06:55:55 +0200

    Mise en place des operations de CRUD, il reste le Delete

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 
+from listings.forms import ContactUsForm, BandForm, ListingForm
 from listings.models import Listing, Band
 
+
 # Create your views here.
-
-
-titres = Listing.objects.all()
-
 
 def band_list(request):
     bands = Band.objects.all()
@@ -22,9 +21,74 @@ def about(request):
     return render(request, 'listings/about.html')
 
 
-def listing(request):
-    return render(request, 'listings/listing.html', {'titres': titres})
+def listing_list(request):
+    titres = Listing.objects.all()
+    return render(request, 'listings/listing_list.html', {'titres': titres})
+
+
+def listing_detail(request, listing_id):
+    titre = Listing.objects.get(id=listing_id)
+    return render(request, 'listings/listing_details.html', {'titre': titre})
+
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band_detail', band.id)
+    else:
+        form = BandForm()
+    return render(request, 'listings/band_create.html', {'form': form})
 
 
 def contact(request):
-    return render(request, 'listings/contact.html')
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+                subject=f"Message from {form.cleaned_data['name'] or 'anonyme'} via MerchEx contact Us Form",
+                message=form.cleaned_data['message'],
+                from_email=form.cleaned_data['email'],
+                recipient_list=['admin@merchex.xyz'],
+            )
+            return redirect("Send Email ...")
+    else:
+        form = ContactUsForm()
+    return render(request, 'listings/contact.html',
+                  {'form': form})
+
+
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing_detail', listing.id)
+    else:
+        form = ListingForm()
+        return render(request, 'listings/listing_create.html', {'form': form})
+
+
+def band_update(request, band_id):
+    band = Band.objects.get(id=band_id)
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect('band_detail', band.id)
+    else:
+        form = BandForm(instance=band)
+        return render(request, 'listings/band_update.html', {'form': form})
+
+
+def listing_update(request, listing_id):
+    ls = Listing.objects.get(id=listing_id)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=ls)
+        if form.is_valid():
+            form.save()
+            return redirect('listing_detail', ls.id)
+    else:
+        form = ListingForm(instance=ls)
+        return render(request, 'listings/listing_update.html', {'form': form})
